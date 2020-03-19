8814ec72837b9ee0f33e96aea05b8b8bc87d39aa
Annie T Bellefleur <anniebellefleur@gmail.com>
2020-03-19 01:23:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,8 +29,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  // array.shift()
-  return array.shift()
+   array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
